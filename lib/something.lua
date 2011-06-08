012e75281374bd5b17f404275b9393c6ba499c02
Daniel Morris <daniel.john.morris@googlemail.com>
2011-06-08 20:32:57 +0100

    Messy code but have pretty good idea of how to do OOP in corona sdk now...

--- lib/something.lua
+++ lib/something.lua
@@ -1,18 +1,86 @@
 --> CLASS NOTES:
 		-- local vars and functions can't be called externally
+		-- no real difference between . and :, but use class.var and class:method
+		-- Calling o:func(a,b,c) is the same as calling o.func(o,a,b,c). And defining function o:func(a,b,c) is the same as function o.func(self,a,b,c) where "self" points to the object o.
 
-module (..., package.seeall)
+--> don't do the module package thing for classes (errors if you do)
+-- module (..., package.seeall)
 
-local Something = { funcs = {} }
+--> module prototype - class instances don't get these
+Something = { 
+	class_var1 = -1
+}
 Something.__index = Something
 
-function Something.__call(_, value)
-	return Something:new(value)
+-- this works too
+Something.class_var2 = -2
+
+-- method missing?
+-- function Something.__call(_, value)
+-- 	return Something:new(value)
+-- end
+
+-- constructor
+function Something:new(b, c)
+	local init = { 
+		class_var1 = -1, 
+		b = -2 
+	}
+	-- if b then init.b = b end
+	if c then init.c = c end
+	-- local init = { b = var1 or 22, c = var2 or 33 }
+	return setmetatable(init, self)
 end
 
-function Something:new(value, chained)
-	return setmetatable({ _val = value, chained = chained or false }, self)
+function Something:new_with_hash(params)
+	-- class instance vars with defaults
+	local init = { a = 100, b = 200, c = 300 }
+	
+	-- override any passed params - without validation
+	for k,v in pairs(params) do init[k] = v end
+
+	-- per param behaviour
+	if params[c] then init[c] = c end
+
+	-- local init = { b = var1 or 22, c = var2 or 33 }
+	return setmetatable(init, self)
 end
+
+function Something:new_with_everything(a_or_t, b, c)
+	-- defaults class instance vars
+	local init = { a = 100, b = 200, c = 300}
+
+	-- regular constructor, expecting certain params
+	if a_or_t and b and c then
+		if a_or_t then init["a"] = a_or_t end
+		-- if b then init["b"] = b end
+		-- if c then init["c"] = c end
+
+		-- could also do this (could also extend with an else block, throwing a param-non-present error)
+		if b then init.b = b end
+
+		-- or, if always want to trust params / want errors if not present
+		init["c"] = c
+
+	-- free form constructor using table params
+	elseif type(a_or_t) == "table" then
+		for k,v in pairs(a_or_t) do init[k] = v end
+		
+	-- just use defaults
+	else
+		-- anything actually needed here?
+	end
+
+	return setmetatable(init, self)
+end
+
+function Something:bump(key, amount)
+	self[key] = self[key] + amount
+end
+
+-- function Something:debug()
+-- 	util.print_r(self)
+-- end
 
 -- var1 = "hi"
 -- local var2 = "there"
--- main.lua
+++ main.lua
@@ -1,13 +1,65 @@
 local _ = require("lib/underscore.lua/underscore")
-local util = require("lib/util.lua/util")
+require("lib/util.lua/util")
+require("lib/something")
 
-Something = require("something")
+print("================= APP START =================")
+print()
 
-function func()
-	util.print_r(Something)
-end
 
-func()
+print("==== Class:new(a, b, c) =================")
+print()
+
+print("## Module:")
+print_r(Something)
+
+s1 = Something:new(10, 20)
+s2 = Something:new(10, 20)
+
+print("## Instantiated class #1:")
+print_r(s1)
+
+print("## Instantiated class #2:")
+print_r(s2)
+
+s1:bump("b", 100)
+print("## Increment a var on one instance:")
+print(s1.b)
+print(s2.b)
+print()
+
+print()
+print("==== Class:new_with_hash({}) =================")
+print()
+
+s3 = Something:new_with_hash({b=5, c=5, x=5})
+
+print("## Instantiated class #3:")
+print_r(s3)
+
+print()
+print("==== Class:new_with_everything(*) =================")
+print()
+
+s4 = Something:new_with_everything(9, 9, 9)
+s5 = Something:new_with_everything({b=8, c=8, x=8})
+s6 = Something:new_with_everything()
+
+Something.class_var2 = -22
+
+print("## Instantiated class #4:")
+print_r(s4)
+print("## Instantiated class #5:")
+print_r(s5)
+print("## Instantiated class #6:")
+print_r(s6)
+
+-- function func()
+-- 	-- 
+-- 	-- util.print_r(Something)
+-- 	-- util.print_r(s)
+-- end
+-- 
+-- func()
 
 -- local util = require("util")
 -- local _ = require("lib/underscore/underscore")
